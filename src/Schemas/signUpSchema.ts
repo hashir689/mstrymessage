0c69feae417e464f3600bb8f58d253ec3e3e55f9
hashir689 <hashirhussain689@gmail.com>
2026-05-30 21:01:43 +0500

    first commit

---
+++ src/Schemas/signUpSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be atlease 3 characters")
+    .max(20, "Username must not have more then 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/, "username must not have special character"),
+  email: z.email({ message: "Invalid email address" }),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnection(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to Database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.log("Database Connection failed because of " + error);
+    process.exit(1);
+  }
+}
+
+export default dbConnection;
