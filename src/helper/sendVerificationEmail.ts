1e898b9c8140f02b852211fb5a46ce16ac95be3b
hashir689 <hashirhussain689@gmail.com>
2026-06-01 15:01:33 +0500

    Adding EmailVerification resend Template

---
+++ src/helper/sendVerificationEmail.ts
@@ -0,0 +1,5 @@
+import { resend } from "@/lib/resend";
+import { EmailTemplate } from "../../Emails/EmailVerification";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerficationEmail() {}
