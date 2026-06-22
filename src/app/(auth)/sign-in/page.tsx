9c55de350ad91e2d70a7c2e781ea68af117a87e2
hashir689 <hashirhussain689@gmail.com>
2026-06-22 22:57:09 +0500

    Adding userForm

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,64 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Controller, useForm } from "react-hook-form";
-import { toast } from "sonner";
+import { useForm } from "react-hook-form";
 import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { fa } from "zod/v4/locales";
+import { toast } from "sonner";
+import { useDebounceValue } from "usehooks-ts";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/Schemas/signUpSchema";
+import axios from "axios";
 
 const page = () => {
-  return <div></div>;
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const deboundusername = useDebounceValue(username, 500);
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (deboundusername) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
+        try {
+          const response = await axios.get(
+            `/api/check-username-unique?username=${deboundusername}`,
+          );
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          setUsernameMessage("Unexpected error occure");
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    };
+    checkUsernameUnique();
+  }, [deboundusername]);
+  return (
+    <div>
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast("Event has been created", { position: "bottom-right" })
+        }
+      >
+        Bottom Right
+      </Button>
+    </div>
+  );
 };
 
 export default page;
